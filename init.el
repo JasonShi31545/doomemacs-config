ec7ce4733410159e3776447e319681f87c074ca3
JasonShi31545Personal <shibingzhao@outlook.com>
2024-09-09 19:58:24 +0800

    Change company to corfu

--- init.el
+++ init.el
@@ -20,8 +20,9 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       ;;company           ; the ultimate code completion backend
        ;;(corfu +orderless)
+       corfu
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
